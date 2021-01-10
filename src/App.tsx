407f4b3f991a788d5e5471adaf160657d8167d0b
asash3 <9412135+asash3@users.noreply.github.com>
2021-01-11 08:57:42 +0900

    fixup! [add] login page to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 function App() {
   return (
